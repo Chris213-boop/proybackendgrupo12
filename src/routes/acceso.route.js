43492a1d4f5ecced709fefa4cf41dd56274c9692
RicardoJulianFlores <131063701+RicardoJulianFlores@users.noreply.github.com>
2026-07-11 08:15:13 -0300

    privo accesos

--- src/routes/acceso.route.js
+++ src/routes/acceso.route.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const accesoCtrl = require('../controllers/acceso.controller');
+const autCtrl = require('./../controllers/auth.controller');
 
 
-router.post('/', accesoCtrl.registrarAcceso);       
-router.get('/', accesoCtrl.obtenerAccesos);        
-router.get('/:id', accesoCtrl.obtenerAccesoPorId); 
-router.delete('/:id', accesoCtrl.eliminarAcceso);  
+router.post('/', [autCtrl.verifyToken, autCtrl.isEmpleado], accesoCtrl.registrarAcceso);       
+router.get('/', [autCtrl.verifyToken, autCtrl.isEmpleado], accesoCtrl.obtenerAccesos);        
+router.get('/:id', [autCtrl.verifyToken, autCtrl.isEmpleado], accesoCtrl.obtenerAccesoPorId); 
+router.delete('/:id', [autCtrl.verifyToken, autCtrl.isEmpleado], accesoCtrl.eliminarAcceso);  
 
 module.exports = router;
